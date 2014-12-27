4700c0968ac6cd3df538c119dd853dcf63e8d980
Timo de Winter <timodewinter@gmail.com>
2014-12-27 15:34:45 +0100

    Addeded comment

--- lib/extras.php
+++ lib/extras.php
@@ -6,3 +6,7 @@ function roots_excerpt_more() {
   return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'roots') . '</a>';
 }
 add_filter('excerpt_more', 'roots_excerpt_more');
+
+
+
+// add custom code here //
